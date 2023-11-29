6da60a4c2d58855fbf2c17d4f33747b9c8155167
Ralf Zeilstra <ralfzeils@gmail.com>
2023-11-29 20:49:10 +0100

    Added uppercase function

--- Opdracht 4 STL Algorithms/main.cpp
+++ Opdracht 4 STL Algorithms/main.cpp
@@ -3,10 +3,29 @@
 #include <functional>
 #include <algorithm>
 
+template<typename T>
+void PrintListResults(std::string label, std::vector<T> const& vec)
+{
+    std::cout << label << ":" << std::endl;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        std::cout << vec[i] << std::endl;
+    }
+}
+
 int main() {
-    std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
     // gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
-    // 1) de vector in 2 nieuwe vectoren te splitsen: 1 met alles wat alfabetisch voor 'purple' komt, 1 met alles er na
+    {
+        std::vector<std::string> colours{ "red", "green", "white", "blue", "orange", "green", "orange", "black", "purple" };
+        // 1) de vector in 2 nieuwe vectoren te splitsen: 1 met alles wat alfabetisch voor 'purple' komt, 1 met alles er na
+
+        for (std::string& s : colours) {
+            std::transform(s.begin(), s.end(), s.begin(),
+                [](unsigned char c) { return std::toupper(c); });
+        }
+
+        PrintListResults("Make strings uppercase", colours);
+    }
     {
         std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
         // 2) alle elementen UPPERCASE te maken.
@@ -21,7 +40,12 @@ int main() {
     {
         std::vector<double> numbers{10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635};
         // 1) alle negatieve elementen te verwijderen
-        std::remove_if(numbers.begin(), numbers.end(), [](double i) { return i > 0; });
+        numbers.erase(
+            remove_if(numbers.begin(), numbers.end(), [](double i) { return i < 0; }), 
+            numbers.end()
+        );
+
+        PrintListResults("Remove negative results", numbers);
     }
 
     {
@@ -33,6 +57,8 @@ int main() {
         std::vector<double> numbers{10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635};
         // 3) de som, het gemiddelde, en het product van alle getallen te berekenen
     }
+
+    
 
     return 0;
 }
