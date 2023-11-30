53fbb7faa1130e13d04728a0b43c2cc16cb711f8
Ralf Zeilstra <ralfzeils@gmail.com>
2023-11-30 11:40:04 +0100

    Added more solutions

--- Opdracht 4 STL Algorithms/main.cpp
+++ Opdracht 4 STL Algorithms/main.cpp
@@ -6,7 +6,7 @@
 template<typename T>
 void PrintListResults(std::string label, std::vector<T> const& vec)
 {
-    std::cout << label << ":" << std::endl;
+    std::cout << std::endl << label << ":" << std::endl;
     for (int i = 0; i < vec.size(); i++)
     {
         std::cout << vec[i] << std::endl;
@@ -18,7 +18,21 @@ int main() {
     {
         std::vector<std::string> colours{ "red", "green", "white", "blue", "orange", "green", "orange", "black", "purple" };
         // 1) de vector in 2 nieuwe vectoren te splitsen: 1 met alles wat alfabetisch voor 'purple' komt, 1 met alles er na
+        std::sort(colours.begin(), colours.end());
+        
+        // Create new vectors and assign the sizes 
+        unsigned splitSize = std::count_if(colours.begin(), colours.end(), [](std::string s) { return s < "p"; });
+        std::vector<std::string> colours1(splitSize), colours2(colours.size() - splitSize);
 
+        std::partition_copy(colours.begin(), colours.end(), colours1.begin(), colours2.begin(), [](std::string s) { return s < "p"; });
+
+        PrintListResults("Split vectors alphabetically part 1", colours1);
+        PrintListResults("Split vectors alphabetically part 2", colours2);
+    }
+    {
+        std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
+        // 2) alle elementen UPPERCASE te maken.
+       
         for (std::string& s : colours) {
             std::transform(s.begin(), s.end(), s.begin(),
                 [](unsigned char c) { return std::toupper(c); });
@@ -28,12 +42,12 @@ int main() {
     }
     {
         std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
-        // 2) alle elementen UPPERCASE te maken.
-       
-    }
-    {
-        std::vector<std::string> colours{"red", "green", "white", "blue", "orange", "green", "orange", "black", "purple"};
         // 3) alle dubbele te verwijderen
+
+        std::sort(colours.begin(), colours.end());
+        colours.erase(std::unique(colours.begin(), colours.end()), colours.end());
+
+        PrintListResults("Remove duplicate elements", colours);
     }
 
     // gebruik functies uit <algorithm> en <functional> om (steeds vanuit een ORIGINELE copie van deze vector)
@@ -51,14 +65,15 @@ int main() {
     {
         std::vector<double> numbers{10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635};
         // 2) voor alle elementen te bepalen of ze even of oneven zijn
+        
     }
 
     {
         std::vector<double> numbers{10, 324422, 6, -23, 234.5, 654.1, 3.1242, -9.23, 635};
         // 3) de som, het gemiddelde, en het product van alle getallen te berekenen
-    }
 
-    
+        double sum, avarage, product;
+    }
 
     return 0;
 }
